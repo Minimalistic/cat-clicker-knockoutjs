f76e972c164b13f8998754bf215037de9b004915
Jason Marsh <regularly.jason@gmail.com>
2017-06-08 10:56:34 -0500

    'With' implimented

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
-var ViewModel = function() {
+var Cat = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
-    self.nicknames = ko.observableArray([
-        { nickname: 'Beef' },
-        { nickname: 'Gob' }, 
-        { nickname: 'Lucifer' },
-        { nickname: 'Mop' }
-        ]);
-    this.level = ko.observable('Newborn');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigsomething')
+    this.nicknames = ko.observableArray([
+        'Beef',
+        'Gob', 
+        'Lucifer',
+        'Mop' 
+        ]);
+    this.level = ko.observable('Newborn');
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
 
     this.levelCounter = ko.computed(function() {
         if(this.clickCount()<10){
@@ -27,5 +24,15 @@ var ViewModel = function() {
         }
     }, this);
 }
+
+var ViewModel = function() {
+
+    this.currentCat = ko.observable( new Cat() );
+
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+
+};
 
 ko.applyBindings(new ViewModel());
